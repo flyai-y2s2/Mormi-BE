3db14b8e27b7ffe35cd65674ffdcb4a4e170eaa8
julianyeong <julia486025@gmail.com>
2026-08-12 17:06:39 +0900

    chore: infra setting

--- src/test/java/com/mormi/backend/BackendApplicationTests.java
+++ src/test/java/com/mormi/backend/BackendApplicationTests.java
@@ -2,9 +2,20 @@ package com.mormi.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class BackendApplicationTests {
+
+	// TODO: AWS RDS PostgreSQL major version 확인 후 동일 태그로 맞출 것.
+	@Container
+	@ServiceConnection
+	static PostgreSQLContainer<?> postgres =
+			new PostgreSQLContainer<>("postgres:16-alpine");
 
 	@Test
 	void contextLoads() {
